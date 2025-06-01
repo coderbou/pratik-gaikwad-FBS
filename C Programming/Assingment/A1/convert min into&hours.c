f4e03bd21a19bc2ba200e809a50783568debcf9b
coderbou <pratikgaikwad987654@gmail.com>
2025-06-01 22:37:34 +0530

    Add files via upload

---
+++ C Programming/Assingment/A1/convert min into&hours.c
@@ -0,0 +1,10 @@
+void main() {
+    int totalMinutes = 190;
+    int hours, minutes;
+
+    hours = totalMinutes / 60;
+    minutes = totalMinutes % 60;
+
+    printf("%d minutes is equal to %d hour(s) and %d minute(s).\n", totalMinutes, hours, minutes);
+
+}
