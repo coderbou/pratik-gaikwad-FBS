2d07f08170812dfcc9a9335bbc49ea10000763ff
coderbou <pratikgaikwad987654@gmail.com>
2025-06-01 22:30:44 +0530

    Add files via upload

---
+++ C Programming/Assingment/A1/covert temp Celsius.c
@@ -0,0 +1,14 @@
+void main()
+{
+
+    float celsius = 25.0; // You can change this value to test with different inputs
+    float fahrenheit;
+
+    fahrenheit = (celsius * 9.0 / 5.0) + 32;
+
+    printf("%.2f Celsius is %.2f Fahrenheit\n", celsius, fahrenheit);
+
+    
+
+
+}
