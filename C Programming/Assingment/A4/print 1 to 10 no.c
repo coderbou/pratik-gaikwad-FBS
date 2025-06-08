a964859a2b50a0aeb2a9fc5b7d7b31ba1e990440
coderbou <pratikgaikwad987654@gmail.com>
2025-06-08 22:19:42 +0530

    Add files via upload

---
+++ C Programming/Assingment/A4/print 1 to 10 no.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main()
+{
+	int no = 1;
+	
+	while(no <= 10)
+	{
+		printf("%d\n",no);
+		
+		no++;
+	}
+}
---
+++ C Programming/Assingment/A4/start to end sum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void main()
+{
+	int start = 1;
+	int end = 5;
+	int sum=0;
+	int i = start;
+	
+	while(i <= end)
+	{
+		sum += i;
+		i++;
+	}
+	
+	printf("%d",sum);
+}
