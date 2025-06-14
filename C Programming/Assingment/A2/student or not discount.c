2b0c78b05033a736a0a6d16710d6437e19cfc1b4
coderbou <pratikgaikwad987654@gmail.com>
2025-06-14 10:59:45 +0530

    Update student or not discount.c

--- C Programming/Assingment/A2/student or not discount.c
+++ C Programming/Assingment/A2/student or not discount.c
@@ -4,14 +4,12 @@ void main() {
     float price, discount = 0, final_price;
     char student;
 
-    // Accepting input from the user
     printf("Enter the purchase amount: ");
     scanf("%f", &price);
 
     printf("Are you a student? (y/n): ");
-    scanf(" %c", &student);  // space before %c to consume any leftover newline character
+    scanf(" %c", &student); 
 
-    // Applying conditions using if-else only
     if (student == 'y' || student == 'Y') {
         if (price > 500) {
             discount = 0.20 * price;
@@ -28,7 +26,6 @@ void main() {
 
     final_price = price - discount;
 
-    // Output
     printf("Discount: %.2f\n", discount);
     printf("Final price to be paid: %.2f\n", final_price);
 
