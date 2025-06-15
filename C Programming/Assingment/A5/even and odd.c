3b8dd862ee5d865725547d363fdcaae061b63e57
coderbou <pratikgaikwad987654@gmail.com>
2025-06-15 19:04:25 +0530

    Add files via upload

---
+++ C Programming/Assingment/A5/even and odd.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main() 
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0) 
+	{
+        printf("Even number\n");
+    } 
+	else 
+	{
+        printf("Odd number\n");
+    }
+}
