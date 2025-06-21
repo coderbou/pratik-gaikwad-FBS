7fcb1755aeb3412890051045bc715072e6f37a46
coderbou <pratikgaikwad987654@gmail.com>
2025-06-21 13:18:17 +0530

    Add files via upload

---
+++ C Programming/Assingment/A6/half pyramid using numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void main()
+{
+	int no;
+	printf("Enter the number : ");
+	scanf("%d",&no);
+	
+	for (int i = 1; i <= no; i++)
+	{
+		for(int j = 1; j <= i + 1; j++)
+		{
+			printf("%d",j);
+		}
+		printf("\n");
+	}
+}
